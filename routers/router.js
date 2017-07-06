c429147c0722269479fa738369255320971a3cb8
niminjie <niminjie@baidu.com>
2017-07-06 21:46:00 +0800

    Add erji

--- routers/router.js
+++ routers/router.js
@@ -6,10 +6,13 @@ import cheerio from 'cheerio'
 import AiWanRSS from '../lib/RSS/AiWan/AiWanRSS'
 import AiWanItem from '../lib/RSS/AiWan/AiWanItem'
 
+import ErjiRSS from '../lib/RSS/Erji/ErjiRSS'
+import ErjiItem from '../lib/RSS/Erji/ErjiItem'
+
 const router = Router()
-const fetchUrl = 'http://play.163.com/rank/'
 
 router.get('/aiwan', async (ctx, next) => {
+    const aiwanUrl = 'http://play.163.com/rank/'
     const html = await fetchHtml(fetchUrl)
 
     const $html = cheerio.load(html, { decodeEntities: false })
@@ -20,6 +23,24 @@ router.get('/aiwan', async (ctx, next) => {
             site_url: 'http://example.com'
         },
         ItemType: AiWanItem,
+    })
+
+    ctx.type = 'text/xml; charset=utf-8'
+    ctx.body = rss.xml(true)
+})
+
+router.get('/erji', async (ctx, next) => {
+    const erjiUrl = 'http://www.erji.net/forum.php?mod=forumdisplay&fid=2'
+    const html = await fetchHtml(erjiUrl);
+
+    const $html = cheerio.load(html, { decodeEntities: false })
+    const rss = new ErjiRSS($html, {
+        feedOptions: {
+            title: '耳机大家坛',
+            feed_url: 'http://www.erji.net/forum.php',
+            site_url: 'http://www.erji.net/forum.php'
+        },
+        ItemType: ErjiItem,
     })
 
     ctx.type = 'text/xml; charset=utf-8'
